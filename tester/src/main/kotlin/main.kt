f1597e268e7181f01dec6f10feab487881f691d2
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-10-30 20:31:08 -0300

    First Commit!

---
+++ tester/src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+package net_perfect_tea_teste
+
+
+class TesterPerfectTeaNet {
+    val tester = "NET_TESTER_PERFECT_TEA"
+    val tea = "PerfectTea Work"
+}
+
+fun main() {
+    val teacodevalue = 1293023496 // This code is not private
+    println(teacodevalue)
+}
